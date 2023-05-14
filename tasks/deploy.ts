96bfe5155e04309f35904bcf0c16ab8b5ce2a043
shadowsupercoder <shadowsupercoder@example.com>
2023-05-14 07:48:28 -0400

    updated the task for the Crowdfunding deployment

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -2,13 +2,19 @@ import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { Crowdfunding } from "../typechain-types";
 
-task("deploy", "Deploy Crowdfunding smart contract").setAction(
-  async (args: any, hre: HardhatRuntimeEnvironment) => {
-    const Crowdfunding =
-      await hre.ethers.getContractFactory("Crowdfunding");
-    const contract = await Crowdfunding.deploy();
+task("deploy", "Deploy Crowdfunding smart contract")
+  .addParam(
+    "mainToken",
+    "The ERC-20 token is used as a mechanism to accomplish a fundraising"
+  )
+  .addParam("owner", "The owner address for the contract")
+  .setAction(async (args: any, hre: HardhatRuntimeEnvironment) => {
+    const Crowdfunding = await hre.ethers.getContractFactory("Crowdfunding");
+    const mainTokenAddress = hre.ethers.utils.getAddress(args.mainToken);
+    const ownerAddress = hre.ethers.utils.getAddress(args.owner);
+
+    const contract = await Crowdfunding.deploy(mainTokenAddress, ownerAddress);
     await contract.deployed();
 
     console.log(`Crowdfunding deployed at: ${contract.address}`);
-  }
-);
+  });
